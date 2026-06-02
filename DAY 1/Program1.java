985bb46054ce2200a4c2de6aad1c4a08219abee3
Suryansh Singh Ujjain <suryansh.ujjain2607@gmail.com>
2026-06-02 20:41:53 +0530

    Added Day 1 Java programs

--- DAY 1/Program1.java
+++ DAY 1/Program1.java
@@ -1,14 +1,13 @@
-import java.util.Scanner;  
+import java.util.Scanner;
 public class Program1 {
-    public static void main() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
         int sum = 0;
-        for (int i = 1; i <= n; i++) {
+        for(int i = 1; i <= n; i++) {
             sum = sum + i;
         }
-        System.out.println("Sum of first " + n + " natural numbers is: " + sum);
-        sc.close();
+        System.out.println("Sum = " + sum);
     }
 }
