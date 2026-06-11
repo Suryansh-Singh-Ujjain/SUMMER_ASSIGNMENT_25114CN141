138feb0f6b6ff8a3f55aaa99bb61bfedcc70d795
Suryansh Singh Ujjain <suryansh.ujjain2607@gmail.com>
2026-06-11 21:13:30 +0530

    Added Day 11 programs

---
+++ DAY 11/Program43.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Program43 {
+    static int isPrime(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if (isPrime(num) == 1) {
+            System.out.println("Prime Number");
+        } else {
+            System.out.println("Not a Prime Number");
+        }
+        sc.close();
+    }
+}
