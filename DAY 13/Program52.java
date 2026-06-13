c6d2e2cf8cd02d98d2590c16923d5ccf56df1241
Suryansh Singh Ujjain <suryansh.ujjain2607@gmail.com>
2026-06-13 20:09:59 +0530

    Added Day 13 programs

---
+++ DAY 13/Program52.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Program52 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int even = 0;
+        int odd = 0;
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.println("Number of even elements = " + even);
+        System.out.println("Number of odd elements = " + odd);
+        sc.close();
+    }
+}
