1326905c53210a1d66b79877e480c1d033a7d4c3
Suryansh Singh Ujjain <suryansh.ujjain2607@gmail.com>
2026-06-22 00:50:08 +0530

    Added Day 21 programs

---
+++ DAY 21/Program81.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Program81 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        int count = 0;
+        try {
+            while (true) {
+                str.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {
+        }
+        System.out.println("Length of string = " + count);
+        sc.close();
+    }
+}
---
+++ DAY 21/Program82.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Program82 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        System.out.println("Reversed string: " + reverse);
+        sc.close();
+    }
+}
